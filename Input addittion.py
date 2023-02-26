a1864123a60af4cc590e11e4d9db74f70e8e4fbb
MahomedSaajidCassim <38559799+MahomedSaajidCassim@users.noreply.github.com>
2023-02-26 19:30:40 +0200

    Delete Input addittion.py

--- Input addittion.py
+++
@@ -1,8 +0,0 @@
-def sum (a, b):
-    return a + b
-
-a =int(input("Enter the first number to be added: "))
-b =int(input("Enter the second number to be added: "))
-k = sum(a, b)
-
-print ('The total of both numbers are: ' , k )
