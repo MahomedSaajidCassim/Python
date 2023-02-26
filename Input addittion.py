d0271aac77c517765db0f5d06bbc82df802877e2
MahomedSaajidCassim <38559799+MahomedSaajidCassim@users.noreply.github.com>
2023-02-26 19:32:35 +0200

    Add files via upload

---
+++ Input addittion.py
@@ -0,0 +1,8 @@
+def sum (a, b):
+    return a + b
+
+a =int(input("Enter the first number to be added: "))
+b =int(input("Enter the second number to be added: "))
+k = sum(a, b)
+
+print ('The total of both numbers are: ' , k )
